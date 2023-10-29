03be494b114f8aefdcca9edca29c7ed3ce2ad06b
Dys-lexi <141734152+Dys-lexi@users.noreply.github.com>
2023-10-29 23:43:02 +0000

    Update wsmixer.py

--- wsmixer.py
+++ wsmixer.py
@@ -1,7 +1,5 @@
-
-
 from pycaw.pycaw import AudioUtilities
-import random 
+import random
 import requests
 import time
 import colorgetter
@@ -9,38 +7,45 @@ import asyncio
 import socketio
 import random
 import keyboard
-USERNAME = ""
-PASSWORD = ""
+USERNAME = "pff"
+PASSWORD = "pffed"
 
-#case specific application names btw (exe name but without .exe)
-keys = ["page up","page down","0","9","insert","delete"]
-keylinks = ["games","browsers","chats"]
-hotkeys = {"browsers":["waterfox","firefox","chrome"],"games":["Terraria","dotnet","r5apex","FSD-Win64-Shipping","Titanfall2","Hl2"],"chats":["Discord"]}
+# case specific application names btw (exe name but without .exe)
+keys = ["page up", "page down", "home", "end", "insert", "delete"]
+keylinks = ["games", "browsers", "chats"]
+hotkeys = {"browsers": ["waterfox", "firefox", "chrome"], "games": ["Terraria", "dotnet", "r5apex", "FSD-Win64-Shipping",
+                                                                    "Titanfall2", "Overwatch", "OuterWilds", "hl2","Discovery", "AShortHike", "factorio", "Goose Goose Duck"], "chats": ["Discord", "DiscordPTB"]}
 
 
 sessions = False
 
 token = False
-sio = socketio.AsyncClient()
+sio = socketio.AsyncClient()#reconnection=True, reconnection_attempts=22222, reconnection_delay=1, reconnection_delay_max=5)
+
+
+
+
 async def mouseconnect():
     global token
-    try:    
-        req = requests.post('https://xixya.com/api/log', json = {"username":USERNAME,"password":PASSWORD},timeout=8)
-        
+    try:
+        req = requests.post('https://xixya.com/api/log',
+                            json={"username": USERNAME, "password": PASSWORD}, timeout=8)
+
         req = req.json()
     except Exception as e:
         print(e)
         time.sleep(1)
         return
-    print("login status:",req["message"])
-    if req["message"]!="ok":
+    print("login status:", req["message"])
+    if req["message"] != "ok":
         time.sleep(1)
         print(req["message"])
-    
+
     token = req["token"]
-    req = requests.post('https://xixya.com/api/mixerhotkeys', json = {"token":token,"hotkeys":hotkeys},timeout=8)
+    req = requests.post('https://xixya.com/api/mixerhotkeys',
+                        json={"token": token, "hotkeys": hotkeys}, timeout=8)
     req = req.json()
-    if req["message"]!="ok":
+    if req["message"] != "ok":
         print(req["message"])
     else:
         print("hotkeys set")
@@ -48,14 +53,25 @@ async def mouseconnect():
         await sio.connect('https://xixya.com', socketio_path='/api/socket.io/')
     except socketio.exceptions.ConnectionError as e:
         print(f"Connection error: {e}")
-    await sio.emit("/api/connect","mixerclient")
+    await sio.emit("/api/connect", "mixerclient")
     await sio.emit("/api/roominit", {'id': token})
     print("Connected!")
 
-    #await sio.emit("/api/audioset",{"token":token,"audio":{"waterfox":"100"}})
+    # await sio.emit("/api/audioset",{"token":token,"audio":{"waterfox":"100"}})
+@sio.event
+async def disconnect():
+    print("Disconnected from server.")
+    await attempt_reconnect()
 
-    
-
+async def attempt_reconnect():
+    while True:
+        try:
+            print("Attempting to reconnect...")
+            await mouseconnect()
+            break
+        except Exception as e:
+            print(f"Reconnection failed: {e}")
+            await asyncio.sleep(5)
 
 @sio.on("setvolume")
 def setvolume(volume):
@@ -66,28 +82,30 @@ def setvolume(volume):
     for app in w:
         names.append(app.Process.name())
     levels = volume
-            
 
     convertedlevels = {}
-    for i in range (len(levels)):
-        convertedlevels[list(levels.keys())[i]+".exe"] = levels[list(levels.keys())[i]]
+    for i in range(len(levels)):
+        convertedlevels[list(levels.keys())[i] +
+                        ".exe"] = levels[list(levels.keys())[i]]
     levels = convertedlevels
     print(levels)
-        
+
     indexes = {}
-    for i in range (len(names)):
-        for j in range (len(levels)):
+    for i in range(len(names)):
+        for j in range(len(levels)):
             if list(levels.keys())[j] == names[i]:
-                indexes[i]=(levels[list(levels.keys())[j]])
-    setvols(indexes,w)
+                indexes[i] = (levels[list(levels.keys())[j]])
+    setvols(indexes, w)
 
 
 async def waitforkey(keys):
-    #browsers, chats, games
-    b=0
+    # browsers, chats, games
+    b = 0
     while b not in keys:
-        b= await asyncio.to_thread(keyboard.read_key)
+        b = await asyncio.to_thread(keyboard.read_key)
     return b
+
+
 async def macrokeys():
     mutliplier = 1
     while True:
@@ -96,20 +114,21 @@ async def macrokeys():
         increment = 0
         startedat = time.time()
         try:
-            if startedat - lasttime < 0.21:
-                mutliplier+=1
+            if startedat - lasttime < 0.28:
+                mutliplier += 1
             else:
-                mutliplier=1
-        except:pass
+                mutliplier = 1
+        except:
+            pass
         for i in range(len(keys)):
             if keys[i] == key:
                 target = keylinks[i//2]
-                increment = ((((i%2)-1)*-1)*2-1)*mutliplier
+                increment = ((((i % 2)-1)*-1)*2-1)*mutliplier
 
-        await sio.emit("/api/incrementmixer",{"token":token,"target":target,"increment":increment})
+        await sio.emit("/api/incrementmixer", {"token": token, "target": target, "increment": increment})
         lasttime = startedat
         await asyncio.sleep(0.15)
-        
+
 
 def set_mixer_level(session, volume):
     volume_control = session.SimpleAudioVolume
@@ -117,27 +136,33 @@ def set_mixer_level(session, volume):
     volume_control.SetMasterVolume(volume, None)
 
 
-
 def getaudioinfo():
     sessions = AudioUtilities.GetAllSessions()
     running_apps = []
     for session in sessions:
-            if session.Process and session.Process.name():
-                running_apps.append(session)
+        if session.Process and session.Process.name():
+            running_apps.append(session)
     return running_apps
+
+
 def setvols(indexes, w):
     d = []
     for i in w:
-                d.append(i.Process.name())
+        d.append(i.Process.name())
     for index in range(len(w)):
         if hasattr(w[index], 'SimpleAudioVolume'):
             volume_control = w[index].SimpleAudioVolume
             try:
-                set_mixer_level(w[index],round(int(indexes[index]) / 100,2))
-            except:pass
+                set_mixer_level(w[index], round(int(indexes[index]) / 100, 2))
+            except:
+                pass
         else:
-            print("Volume control not available for", w[index]) 
+            print("Volume control not available for", w[index])
+
+
 colors = {}
+
+
 async def main():
     global sessions
     asyncio.create_task(mouseconnect())
@@ -149,33 +174,38 @@ async def main():
         apps = {}
         names = []
         for app in w:
-            if app.Process.name() not in list(colors.keys()): 
-                colors[app.Process.name()] = colorgetter.get_most_common_color(app.Process.exe())
-            
-            if colors[app.Process.name()] != (-2,-1,-1):
-                apps[app.Process.name().split(".exe")[0]] = [round(app.SimpleAudioVolume.GetMasterVolume(),2),colors[app.Process.name()]]
+            if app.Process.name() not in list(colors.keys()):
+                colors[app.Process.name()] = colorgetter.get_most_common_color(
+                    app.Process.exe())
+
+            if colors[app.Process.name()] != (-2, -1, -1):
+                apps[app.Process.name().split(".exe")[0]] = [round(
+                    app.SimpleAudioVolume.GetMasterVolume(), 2), colors[app.Process.name()]]
             names.append(app.Process.name())
         try:
-        
-            r = requests.post('https://xixya.com/api/mixerargs', json = {"apps":apps,"token":token,"type":"endclient"},timeout=6)
-            if r.json()["message"]!="ok":
-                print(r.json()["message"])
+            r = requests.post('https://xixya.com/api/mixerargs',
+                              json={"apps": apps, "token": token, "type": "endclient"}, timeout=6)
+                   
+            if r.json()["message"] != "ok" or failed == True:
                 failed = True
-                d+=1
-            failed = False
+                print(r.json()["message"])
+                d += 1
         except Exception as e:
             print("cannot send request to server")
             if failed:
                 print("http reconnected, starting socket reconnect")
                 await asyncio.sleep(1)
                 await sio.disconnect()
-                
+
                 await asyncio.sleep(5)
-                #sio.clear()
-                #sio = socketio.AsyncClient()
+                # sio.clear()
+                # sio = socketio.AsyncClient()
                 asyncio.create_task(mouseconnect())
                 print("socket reattempt finished")
                 failed = False
+            else:
+                print("waiting for succsesful http to reconnect")
+                failed = True
         await asyncio.sleep(5)
 
 asyncio.run(main())
